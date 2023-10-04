20ba2aabd75fc32c76cf4cf0acd35ff073e3e232
Ankit Kumaar <79037067+ankitkumaar@users.noreply.github.com>
2023-10-05 00:19:37 +0530

    Added solution - LeetHub

--- Roman Number to Integer - GFG/roman-number-to-integer.java
+++ Roman Number to Integer - GFG/roman-number-to-integer.java
@@ -27,6 +27,7 @@ class Solution
     // Finds decimal value of a given roman numeral
     public int romanToDecimal(String str) 
     {
+        // Create HashMap to store integer equivalent of roman
         HashMap<Character, Integer> map = new HashMap<>();
         
         map.put('I', 1);
@@ -37,26 +38,35 @@ class Solution
         map.put('D', 500);
         map.put('M', 1000);
         
+        // If length of string is equal to 1
         if(str.length() == 1)
             return map.get(str.charAt(0));
-            
+        
         int sum = 0;
         char ch1 = 0, ch2 = 0;
+        // Iterate loop till second last element of array
         for(int i=0; i<str.length()-1; i++)
         {
+            // Extract character at index i and i+1
             ch1 = str.charAt(i);
             ch2 = str.charAt(i+1);
             
+            // If character at index i is greater than character at index i+1
+            // Add its value to sum
             if (map.get(ch1) >= map.get(ch2))
             {
                 sum += map.get(ch1);
             }
+            // Else if character at index i is smaller than character at index i+1
+            // Substract its value form sum
             else
             {
                 sum -= map.get(ch1);
             }
         }
+        // Handle sum for last element of array
         sum = sum + map.get(ch2);
+        // Return desired sum
         return sum;
     }
 }
